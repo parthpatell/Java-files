fbe83bd90cfdde419fb7383af94a9fa7c977cf48
Parthkumar <43411180+parthpatell@users.noreply.github.com>
2020-08-19 18:08:24 -0400

    GuessingGamefrom1to20
    The program tells if you are lower or higher to the random number. 
    Simple use of while, if and else if loops.

---
+++ Higher or Lower/src/HIorLow.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+import java.util.*;
+
+public class HIorLow {
+	public static  void main(String args[]) {
+		Random rand = new Random(); //Generates a random number.
+		int upperbound = 21; //Sets an upperbound so the random numbers are generated from 0-20. i.e upperbound -1.
+		
+		//Setting the number to less than upperbound.
+		
+		int int_random = rand.nextInt(upperbound);
+		Scanner myObj = new Scanner(System.in); //input command
+		System.out.println("Enter a number in between 1 to 20");
+		//Loop executed till the requirements are met. So user input will be required on each iteration.
+		while(true) {
+		int userin = myObj.nextInt();
+		//The statements for the output and conditions.
+		if(int_random == userin) {
+			System.out.println("Well done you got lucky");
+			
+		}
+		else if (int_random > userin){
+			System.out.println("Try a higher number");
+			
+			
+		}
+		else if (int_random < userin){
+			System.out.println("Try a smaller number");
+			
+		
+		}
+	}
+	}
+}
