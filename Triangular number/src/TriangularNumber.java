9116238a648e4b1a2276f720e559ae7f5c9c4f30
Parthkumar <43411180+parthpatell@users.noreply.github.com>
2020-08-23 13:54:52 -0400

    Add files via upload

---
+++ Triangular number/src/TriangularNumber.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class TriangularNumber {
+	public static void main(String args[]) {
+		
+		for(int x = 1; x <= 10; x++) {
+			System.out.println(x*3);
+						
+		}
+		for(int y = 0; y <= 1000; y++) {
+			System.out.println((y*(y+1))/2);
+			
+		}
+	}
+
+}
